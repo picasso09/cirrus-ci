f07de0d64ce15214da13309e0bf6c4a4912d99e4
picasso09 <sayazaki282901@gmail.com>
2024-07-06 13:49:57 +0700

    RMX2020: bringup
    Signed-off-by: picasso09

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/RMX2185/qassa*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/RMX2185/ && ls qassa*.zip)"
-      rclone copy ~/rom/out/target/product/RMX2185/qassa*.zip RMX2185:qassa -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/RMX2185/ && ls qassa*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/RMX2020/qassa*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/RMX2020/ && ls qassa*.zip)"
+      rclone copy ~/rom/out/target/product/RMX2020/qassa*.zip RMX2185:qassa -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/RMX2020/ && ls qassa*.zip) Uploaded Successfully!"
 fi
